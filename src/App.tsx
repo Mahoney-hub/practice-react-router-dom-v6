c1b1047978af477306a8b14ee4e2102367b4e677
Pavel Vasilev <pv9998286620@yandex.ru>
2022-05-16 18:15:04 +0300

    lesson 1 - router nesting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {NavLink, Route, Routes} from 'react-router-dom';
+import {NavLink, Outlet, Route, Routes} from 'react-router-dom';
 
 const App = () => {
     return (
@@ -8,13 +8,36 @@ const App = () => {
                 <NavLink to={'/'}>main</NavLink>
                 <NavLink to={'/login'}>login</NavLink>
                 <NavLink to={'/profile'}>profile</NavLink>
+                <NavLink to={'/profile/settings'}>settings</NavLink>
+                <NavLink to={'/login/password'}>password</NavLink>
             </div>
 
-            <div>
+            <div className={'content'}>
                 <Routes>
                     <Route path={'/'} element={<div>main</div>}/>
-                    <Route path={'/login'} element={<div>login</div>}/>
-                    <Route path={'/profile'} element={<div>profile</div>}/>
+                    <Route path={'/*'} element={<div>404</div>}/>
+                    // Вложенность первый способ
+                    <Route path={'/profile/*'} element={(
+                        <div>
+                            profile
+
+                            <Routes>
+                                <Route path={'/settings'} element={<div>settings</div>}/>
+                            </Routes>
+                        </div>)}
+                    />
+                    // Вложенность второй способ
+                    <Route path={'/login/*'} element={(
+                        <div>
+                            login
+
+                            <Outlet/>
+                        </div>
+                    )}>
+                        <Route path={'password'} element={<div>password</div>}/>
+                        <Route path={'*'} element={<div>check id</div>}/>
+                        <Route path={':id'} element={<div>id</div>}/>
+                    </Route>
                 </Routes>
             </div>
         </div>
